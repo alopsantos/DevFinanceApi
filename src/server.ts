2a532f1eacb05bd2c36400f8e325d0c4f12fe181
Anderson Lopes dos Santos <anderson_lops@hotmail.com>
2021-04-14 01:19:00 -0300

    Start - arquivos iniciais do projeto

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,15 @@
 import express from "express";
 import 'express-async-errors';
+import cors from 'cors';
 import "./database/connections";
 
 import routes from './routes';
 import errorHandler from "./errors/handler";
 
 const app = express();
-app.use(express.json());
 
+app.use(cors());
+app.use(express.json());
 app.use(routes);
 app.use(errorHandler);
 
